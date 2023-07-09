f5dbfb97c04ba07c07a379706cced80dc4f35b63
Jacob LeCoq <lecoqjacob@gmail.com>
2023-07-08 20:25:59 -0500

    add version endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,10 +1,19 @@
+use axum::extract::State;
+use axum::response::IntoResponse;
 use axum::{routing::get, Router};
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
 use sqlx::PgPool;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn version(State(pool): State<PgPool>) -> impl IntoResponse {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(&pool)
+        .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
 }
 
 #[shuttle_runtime::main]
@@ -14,7 +23,7 @@ async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::Shut
         .await
         .map_err(CustomError::new)?;
 
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/", get(version)).with_state(pool);
 
     Ok(router.into())
 }
