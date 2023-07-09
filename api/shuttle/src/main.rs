d7b55eb808c358f8ed71591ef58679c2a56da436
Jacob LeCoq <lecoqjacob@gmail.com>
2023-07-09 09:58:35 -0500

    added instrumentation

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,10 +1,21 @@
-use axum::extract::State;
-use axum::response::IntoResponse;
-use axum::{routing::get, Router};
-use shuttle_runtime::CustomError;
+use std::time::Duration;
+
+use axum::{
+    body::Bytes,
+    extract::{MatchedPath, State},
+    http::{HeaderMap, Request},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use shuttle_runtime::tracing::instrument;
+use shuttle_runtime::{tracing, CustomError};
 use sqlx::Executor;
 use sqlx::PgPool;
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing::{info_span, Span};
 
+#[instrument(name = "version", skip(pool), fields(db = "postgres"))]
 async fn version(State(pool): State<PgPool>) -> impl IntoResponse {
     let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
         .fetch_one(&pool)
@@ -25,7 +36,46 @@ async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::Shut
 
     let router = Router::new()
         .route("/version", get(version))
-        .with_state(pool);
+        .with_state(pool)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(|_request: &Request<_>, _span: &Span| {
+                    // You can use `_span.record("some_other_field", value)` in one of these
+                    // closures to attach a value to the initially empty field in the info_span
+                    // created above.
+                })
+                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_eos(
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                        // ...
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                        // ...
+                    },
+                ),
+        );
 
     Ok(router.into())
 }
