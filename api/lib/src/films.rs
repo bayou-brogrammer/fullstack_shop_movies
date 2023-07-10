e98aa7625debb9c627c8defe8f5b3539b6f2766e
Jacob LeCoq <lecoqjacob@gmail.com>
2023-07-09 19:12:29 -0500

    serve static files

--- api/lib/src/films.rs
+++ api/lib/src/films.rs
@@ -1,41 +1,108 @@
-use axum::{http::StatusCode, response::IntoResponse, routing, Router};
+use api_repository::film_repository::FilmRepository;
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing, Json, Router,
+};
+use shared::models::{CreateFilm, Film};
 use tracing::instrument;
 
-pub fn service() -> Router {
+pub fn service<R: FilmRepository + Clone>() -> Router<R> {
     Router::new()
         // get all films
-        .route("/", routing::get(get_all))
+        .route("/", routing::get(get_all::<R>))
         // get by id
-        .route("/:film_id", routing::get(get))
+        .route("/:film_id", routing::get(get::<R>))
         // post new film
-        .route("/", routing::post(post))
+        .route("/", routing::post(post::<R>))
         // update film
-        .route("/", routing::put(put))
+        .route("/", routing::put(put::<R>))
         // delete film
-        .route("/:film_id", routing::delete(delete))
+        .route("/:film_id", routing::delete(delete::<R>))
 }
 
-#[instrument(name = "[films] get all")]
-async fn get_all() -> impl IntoResponse {
-    StatusCode::OK
+#[instrument(name = "[films] get all films", skip(film_repo))]
+async fn get_all<R: FilmRepository>(film_repo: State<R>) -> impl IntoResponse {
+    match film_repo.get_films().await {
+        Ok(films) => {
+            tracing::info!("Retrieved all films");
+            Json(films).into_response()
+        }
+        Err(e) => {
+            tracing::error!("Couldn't retrieve films: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+        }
+    }
 }
 
-#[instrument(name = "[films] get")]
-async fn get() -> impl IntoResponse {
-    StatusCode::OK
+#[instrument(name = "[films] get film by id", skip(film_repo))]
+async fn get<R: FilmRepository>(
+    film_repo: State<R>,
+    Path(film_id): Path<uuid::Uuid>,
+) -> impl IntoResponse {
+    match film_repo.get_film(&film_id).await {
+        Ok(film) => match film {
+            Some(film) => {
+                tracing::info!("Retrieved film with id: {}", film_id);
+                Json(film).into_response()
+            }
+            None => {
+                tracing::info!("Film with id: {} not found", film_id);
+                (StatusCode::NOT_FOUND).into_response()
+            }
+        },
+        Err(e) => {
+            tracing::error!("Couldn't retrieve film with id: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+        }
+    }
 }
 
-#[instrument(name = "[films] post")]
-async fn post() -> impl IntoResponse {
-    StatusCode::OK
+#[instrument(name = "[films] create film", skip(film_repo))]
+async fn post<R: FilmRepository>(
+    film_repo: State<R>,
+    create_film: Json<CreateFilm>,
+) -> impl IntoResponse {
+    match film_repo.create_film(&create_film).await {
+        Ok(film) => {
+            tracing::info!("Created film with id: {}", film.id);
+            Json(film).into_response()
+        }
+        Err(e) => {
+            tracing::error!("Couldn't create film: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+        }
+    }
 }
 
-#[instrument(name = "[films] put")]
-async fn put() -> impl IntoResponse {
-    StatusCode::OK
+#[instrument(name = "[films] update film", skip(film_repo))]
+async fn put<R: FilmRepository>(film_repo: State<R>, update_film: Json<Film>) -> impl IntoResponse {
+    match film_repo.update_film(&update_film).await {
+        Ok(film) => {
+            tracing::info!("Updated film with id: {}", film.id);
+            Json(film).into_response()
+        }
+        Err(e) => {
+            tracing::error!("Couldn't update film: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+        }
+    }
 }
 
-#[instrument(name = "[films] delete")]
-async fn delete() -> impl IntoResponse {
-    StatusCode::OK
+#[instrument(name = "[films] delete film", skip(film_repo))]
+async fn delete<R: FilmRepository>(
+    film_repo: State<R>,
+    Path(film_id): Path<uuid::Uuid>,
+) -> impl IntoResponse {
+    match film_repo.delete_film(&film_id).await {
+        Ok(film_id) => {
+            tracing::info!("Deleted film with id: {}", film_id);
+            Json(film_id).into_response()
+        }
+        Err(e) => {
+            tracing::error!("Couldn't update film: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR).into_response()
+        }
+    }
 }
--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,50 +1,67 @@
+use std::path::PathBuf;
+
+use api_repository::film_repository::PostgresFilmRepository;
 use axum::{extract::MatchedPath, http::Request, Router};
 use shuttle_runtime::{tracing, CustomError};
-use sqlx::Executor;
-use sqlx::PgPool;
-use tower_http::trace::{DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use sqlx::{Executor, PgPool};
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::{DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+};
 use tracing::info_span;
 
 #[shuttle_runtime::main]
-async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
+) -> shuttle_axum::ShuttleAxum {
     // initialize the database if not already initialized
     pool.execute(include_str!("../../db/schema.sql"))
         .await
         .map_err(CustomError::new)?;
 
-    let tracing_layer = TraceLayer::new_for_http()
-        .make_span_with(|request: &Request<_>| {
-            // Log the matched route's path (with placeholders not filled in).
-            // Use request.uri() or OriginalUri if you want the real path.
-            let matched_path = request
-                .extensions()
-                .get::<MatchedPath>()
-                .map(MatchedPath::as_str);
-
-            info_span!(
-                "http_request",
-                method = ?request.method(),
-                matched_path,
-                some_other_field = tracing::field::Empty,
-            )
-        })
-        .on_request(DefaultOnRequest::new().level(tracing::Level::INFO))
-        .on_response(
-            DefaultOnResponse::new()
-                .include_headers(true)
-                .level(tracing::Level::INFO),
-        )
-        .on_failure(DefaultOnFailure::new().level(tracing::Level::INFO));
+    // create a film repository. In this case for postgres.
+    let serve_dir = ServeDir::new(static_folder).fallback(ServeFile::new("index.html"));
 
     let router = Router::new()
-        .nest(
-            "/api",
-            Router::new()
-                .with_state(pool)
-                .nest("/health", api_lib::health::service())
-                .nest("/v1/films", api_lib::films::service()),
-        )
-        .layer(tracing_layer);
+        .nest("/api", api_router(pool))
+        .nest_service("/", serve_dir)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(DefaultOnRequest::new().level(tracing::Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .include_headers(true)
+                        .level(tracing::Level::INFO),
+                )
+                .on_failure(DefaultOnFailure::new().level(tracing::Level::INFO)),
+        );
 
     Ok(router.into())
+}
+
+fn api_router(pool: PgPool) -> Router {
+    let film_repository = PostgresFilmRepository::new(pool);
+    Router::new()
+        .nest(
+            "/v1/films",
+            api_lib::films::service::<PostgresFilmRepository>(),
+        )
+        .with_state(film_repository)
+        .nest("/health", api_lib::health::service())
 }
